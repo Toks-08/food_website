788591eee6eadffdb6f1ee59030f1f0059ffc6d1
Tokurah Ibrahim <tokurah10@gmail.com>
2026-02-21 13:05:10 +0100

    linked apps urls to main project url

--- chuks_kitchen/chuks_kitchen/urls.py
+++ chuks_kitchen/chuks_kitchen/urls.py
@@ -20,5 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/',include('accounts.urls')),
-    path('api/', include('menu.urls'))
+    path('api/', include('menu.urls')),
+    path('api/', include('orders.urls')),
+    path('api/', include('payments.urls')),
+    path('api/', include('reviews.urls'))
     ]
