d19fd836689d1c842d79dd4f64b79ad1d54dd255
Tokurah Ibrahim <tokurah10@gmail.com>
2026-02-24 15:37:52 +0100

    final code

--- chuks_kitchen/accounts/models.py
+++ chuks_kitchen/accounts/models.py
@@ -18,19 +18,14 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault(self.is_active,True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+        extra_fields.setdefault("is_active", True)
 
         return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=20, blank=True, unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, unique=True, null=True)
 
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
